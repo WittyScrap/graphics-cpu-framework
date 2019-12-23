a04ef80b88b3c70603283e5eef5077c2fb5ca194
Francesco Litrico <100492443@unimail.derby.ac.uk>
2019-12-23 00:57:58 +0000

    Implemented specular highlights for point lights.

--- PointLight.cpp
+++ PointLight.cpp
@@ -1,4 +1,5 @@
 #include "PointLight.h"
+#include "Camera.h"
 #include <algorithm>
 
 //
@@ -62,17 +63,21 @@ void PointLight::SetAttenuation(const float& value)
 //
 // Calculates the overall contribution of the point light.
 //
-Colour PointLight::CalculateContribution(const Vertex& position, const Vector3& normal, const float& ks, const float& specular)
+Colour PointLight::CalculateContribution(const Vertex& position, const Vector3& normal, const float& roughness, const float& specular)
 {
 	constexpr float a = 0.f;
 	constexpr float c = 0.f;
 
 	const Vector3 lightRay = _position - position;
+	const Vector3 viewRay = Camera::GetMainCamera()->GetPosition() - position;
 
 	const float distance = lightRay.GetMagnitude();
 	const float attenuation = 1 / (a + _attenuation * distance + c * (distance * distance));
 	const float normalDotRay = max(Vector3::Dot(normal, Vector3::NormaliseVector(lightRay)), 0.f);
 	const float finalIntensity = normalDotRay * attenuation;
 
-	return GetIntensity() * finalIntensity;
+	const Vector3 h((lightRay + viewRay) / (lightRay + viewRay).GetMagnitude());
+	const float phongHighlights = specular * static_cast<float>(pow(Vector3::Dot(normal, h), roughness));
+
+	return GetIntensity() * (finalIntensity + phongHighlights);
 }
